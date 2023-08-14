d53f9c09d6776c3eeef7b6a5912bd5fc66007326
m-lois-bl <marion.blayo2023@campus-eni.fr>
2023-08-14 09:23:32 +0200

    Ajout d'un commentaire dans Security Controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    //Route à discuter
     #[Route(path: '/', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
