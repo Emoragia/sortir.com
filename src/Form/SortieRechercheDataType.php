d022c911ddf2e554c65cd1d22cfb540751369986
m-lois-bl <marion.blayo2023@campus-eni.fr>
2023-08-19 14:15:10 +0200

    Ajout de styles sur la page d'accueil (accueil.html.twig) (formulaire de recherche et liste de sorties).

--- src/Form/SortieRechercheDataType.php
+++ src/Form/SortieRechercheDataType.php
@@ -32,42 +32,69 @@ class SortieRechercheDataType extends AbstractType
                 'choice_label'=> 'nom',
                 'label'=> 'Campus :',
                 'placeholder'=> '--Tous les campus--',
+                'row_attr' => [
+                    'class' => 'input-group',
+                ],
                 'required' => false
 
             ])
             ->add('nomRecherche', TextType::class, [
                 'label'=> 'Rechercher',
+                'attr'=>[
+                    'placeholder'=>'Le nom contient...'
+                ],
+                'row_attr' => [
+                    'class' => 'input-group',
+                ],
                 'required'=>false,
             ])
             ->add('borneDateInf', DateType::class, [
                 'label'=>'Entre le ',
                 'widget'=>'single_text',
 //                'time_widget'=>'single_text',
+                'row_attr' => [
+                    'class' => 'input-group',
+                ],
                 'required'=>false
             ])
             ->add('borneDateSup', DateType::class, [
                 'label'=>'et le',
                 'widget'=>'single_text',
+                'row_attr' => [
+                    'class' => 'input-group',
+                ],
 //                'time_widget'=>'single_text',
                 'required'=>false
             ])
             ->add('organisateur', CheckboxType::class, [
                 'label'=> 'Sorties dont je suis l\'organisateur.ice',
+                'label_attr' => [
+                    'class' => 'checkbox-switch',
+                ],
                 'required'=>false
 
             ])
             ->add('inscrit', CheckboxType::class, [
                 'label'=> 'Sorties auxquelles je suis inscrit.e.',
+                'label_attr' => [
+                    'class' => 'checkbox-switch',
+                ],
                 'required'=>false
 
             ])
             ->add('nonInscrit',CheckboxType::class, [
                 'label'=> 'Sorties auxquelles je ne suis pas inscrit.e.',
+                'label_attr' => [
+                    'class' => 'checkbox-switch',
+                ],
                 'required'=>false
 
             ])
             ->add('sortiesPassees', CheckboxType::class, [
                 'label'=> 'Sorties passées.',
+                'label_attr' => [
+                    'class' => 'checkbox-switch',
+                ],
                 'required'=>false
             ])
         ;
