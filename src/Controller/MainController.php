7a406ae39d9f19519aa8f207c4fff9f5389dd641
m-lois-bl <marion.blayo2023@campus-eni.fr>
2023-08-19 14:41:22 +0200

    Fusion de InscriptionController et MainController. Début de tests sur la méthode inscrire().

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,6 +7,7 @@ use App\Entity\Campus;
 use App\Entity\Participant;
 use App\Form\SortieRechercheDataType;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -28,5 +29,25 @@ class MainController extends AbstractController
                'rechercheForm'=> $rechercheForm->createView(),
                'sorties'=>$sorties
        ]);
+    }
+    #[Route('/inscription', name: 'main_inscription')]
+    public function inscrire(): Response
+    {
+//        $sortie = $sortieRepository->find($id);
+//        $sortie->setEtat('Ouvert');
+//        if ($sortie !== setEtat) {
+//
+//
+//            return new Response('Inscription réussie');
+//        } else {
+//            return new Response('Sortie non trouvée', 404);
+//        }
+        $this->addFlash('success', 'Votre inscription a été prise en compte.');
+        return $this->redirectToRoute('main_accueil');
+    }
+    #[Route('/desistement', name:'main_desistement')]
+    public function seDesister()
+    {
+
     }
 }
